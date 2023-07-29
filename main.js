8413f304115bcd4da37d1a154fa750971ffa0189
Dante Gabriel Tokomoto Takatu <113074962+Dante-Gabriel-Tokomoto-Takatu@users.noreply.github.com>
2023-07-29 00:30:04 -0300

    Project No. 2   Starbucks

--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 document.getElementById("projectNo1").addEventListener("click", function() {
     window.location.href = "ProjectNo1/projectNo1.html";
 });
+
+document.getElementById("projectNo2").addEventListener("click", function() {
+    window.location.href = "projectNo2/projectNo2.html";
+});
---
+++ projectNo2/projectNo2.js
@@ -0,0 +1,42 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
